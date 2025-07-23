90973b38dfe2c65452cb42cd637b778713954e16
calitovilla <calitovilla@users.noreply.github.com>
2025-07-23 12:16:56 +0200

    Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import ThemeToggle from './components/ThemeToggle';
+import Card from './components/Card';
 import { generateFakePersons } from './utils/generateFakePersons';
 import type { Person } from './utils/generateFakePersons';
 import './App.css'
@@ -22,26 +23,7 @@ function App() {
       <ThemeToggle />
       {
         persons.map((person, index) => (
-          <div key={index} className="card bg-base-100 w-96 shadow-sm">
-            <div className="card-body">
-              <div className="avatar">
-                <div className="w-24 rounded">
-                  <img src={person.image} />
-                </div>
-              </div>
-              <h2 className="card-title">{person.firstName} {person.lastName}</h2>
-              <p>Gender: {person.gender}</p>
-              <p>Age: {person.age} years old</p>
-              <p>Email: {person.email.toLowerCase()}</p>
-              {person.pet && (
-                <div>
-                  <h3 className="font-bold">Pet Information:</h3>
-                  <p>Name: {person.pet.animalName}</p>
-                  <p>Type: {person.pet.type}</p>
-                </div>
-              )}
-            </div>
-          </div>
+          <Card key={index} person={person} />
         ))
       }
     </>
---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+import type { Person } from '../utils/generateFakePersons';
+
+interface Props {
+	person: Person; 
+}
+
+const Card = ({ person }: Props) => {
+    return (
+          <div className="card bg-base-100 w-96 shadow-sm">
+            <div className="card-body">
+              <div className="avatar">
+                <div className="w-24 rounded">
+                  <img src={person.image} />
+                </div>
+              </div>
+              <h2 className="card-title">{person.firstName} {person.lastName}</h2>
+              <p>Gender: {person.gender}</p>
+              <p>Age: {person.age} years old</p>
+              <p>Email: {person.email.toLowerCase()}</p>
+              {person.pet && (
+                <div>
+                  <h3 className="font-bold">Pet Information:</h3>
+                  <p>Name: {person.pet.animalName}</p>
+                  <p>Type: {person.pet.type}</p>
+                </div>
+              )}
+            </div>
+          </div>
+    )
+}
+
+export default Card;
